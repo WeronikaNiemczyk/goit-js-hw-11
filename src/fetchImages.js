e66b0ce54b13a6057a0e4ac0892e157322575f62
Weronika Niemczyk <weronika.tlusciak@gmail.com>
2023-04-04 17:02:00 +0200

    update

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_KEY = '34850794-1a63b0f1d33e83dba8f53aae2';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const fetchImages = async searchQuery => {
+export const fetchImages = async (searchQuery, page, perPage ) => {
   const response = await axios.get(BASE_URL, {
     params: {
       key: API_KEY,
@@ -10,6 +10,8 @@ export const fetchImages = async searchQuery => {
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page: page,
+      per_page: perPage,
     },
   });
   return response.data.hits;
--- src/index.js
+++ src/index.js
@@ -4,19 +4,28 @@ import { fetchImages } from './fetchImages';
 
 const galleryContainer = document.querySelector('.gallery');
 const searchForm = document.querySelector('#search-form');
+const loadMoreBtn = document.querySelector('.load-more');
+
+let page = 1;
+let perPage = 40;
+let totalHits;
+loadMoreBtn.style.display = 'none';
 
 const searchingWindow = async event => {
   event.preventDefault();
   galleryContainer.innerHTML = '';
   const searchQuery = event.currentTarget.elements.searchQuery.value.trim();
-  // console.log('searchQuery', searchQuery);
+  loadMoreBtn.style.display = 'block';
+  page = 1;
   if (searchQuery === '') {
-    return Notiflix.Notify.failure('Search query cannot be empty');
+    return Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
   }
 
   try {
-    const hits = await fetchImages(searchQuery);
-
+    const hits = await fetchImages(searchQuery, page, perPage);
+    // const { hits, totalHits } = data;
     if (hits.length === 0) {
       return Notiflix.Notify.warning(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -25,10 +34,45 @@ const searchingWindow = async event => {
 
     const cardsMarkup = createGalleryMarkup(hits);
     galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+    if (totalHits > perPage) {
+      loadMoreBtn.style.display = 'block';
+    }
+    if (totalHits <= page * perPage) {
+      loadMoreBtn.style.display = 'none';
+      Notiflix.Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
   } catch (error) {
     console.log(error);
     Notiflix.Notify.failure('Something went wrong. Please try again later.');
   }
 };
 
+const loadMore = async () => {
+  page += 1;
+  // const searchQuery = searchForm.elements.searchQuery.value.trim();
+  try {
+    const data = await fetchImages(searchQuery, page, perPage).then(
+      ({ data }) => {
+        if (data.totalHits === 0) {
+          loadMoreBtn.style.display = 'none';
+
+          return Notiflix.Notify.warning(
+            "We're sorry, but you've reached the end of search results."
+          );
+        } else {
+          const newPages = createGalleryMarkup(data.hits);
+          galleryContainer.insertAdjacentHTML('beforeend', newPages);
+        }
+      }
+    );
+  } catch (error) {
+    Notiflix.Notify.warning('Sorry, error');
+  }
+  console.log(loadMore());
+};
+
 searchForm.addEventListener('submit', searchingWindow);
+loadMoreBtn.addEventListener('click', loadMore);
